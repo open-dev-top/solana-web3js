125dab653b650e02f278942f120a7dfebb8596c2
open-dev-top <opendev.top@gmail.com>
2025-03-03 10:19:57 +0800

    update 02 & 03

--- 03-transfer/transfer_sol.ts
+++ 03-transfer/transfer_sol.ts
@@ -9,11 +9,12 @@ import {
 import fs from "fs";
 
 // 创建RPC连接
-const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+const connection = new Connection("https://devnet.helius-rpc.com/?api-key=5478b127-a6be-4172-87cb-f36d697a8c6b", "confirmed")
+// const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
 // const connection = new Connection("https://mainnet-ams.chainbuff.com", "confirmed");
 
 // 本地导入钱包
-const fromSecretKey = Uint8Array.from(JSON.parse(fs.readFileSync("wallet.json")));
+const fromSecretKey = Uint8Array.from(JSON.parse(fs.readFileSync("wallet.json", "utf-8")));
 // const fromSecretKey = Uint8Array.from(JSON.parse(fs.readFileSync("web3xFMwEPrc92NeeXdAigni95NDnnd2NPuajTirao2.json")));
 const fromWallet = Keypair.fromSecretKey(fromSecretKey);
 
@@ -23,7 +24,7 @@ async function main() {
     const transaction = new Transaction();
 
     // 目标地址
-    const toAddress = new PublicKey('buffaAJKmNLao65TDTUGq8oB9HgxkfPLGqPMFQapotJ');
+    const toAddress = new PublicKey('Fs9yoikaDDrkhZxYcy9RUvzn7ksGxLxdfLdredykeg5S');
 
     // 添加转账指令
     const instruction = SystemProgram.transfer({
@@ -39,7 +40,8 @@ async function main() {
 
     // 发送交易
     const signature = await sendAndConfirmTransaction(connection, transaction, [fromWallet]);
-    console.log(`交易已发送: https://solscan.io/tx/${signature}`);
+    // console.log(`交易已发送: https://solscan.io/tx/${signature}`);
+    console.log(`交易已发送: https://solscan.io/tx/${signature}?cluster=testnet`);
 }
 
 main();
